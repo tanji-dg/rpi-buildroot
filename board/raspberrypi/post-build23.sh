929afcad186b1b94eabb066ff0b6348a1c5d8fb5
Hideki Tanji <tanji@daitogiken.com>
2018-02-13 11:51:42 +0900

    raspberry pi zeroとpi2、pi3とトリプルブートの実現。

--- board/raspberrypi/post-build23.sh
+++ board/raspberrypi/post-build23.sh
@@ -6,8 +6,8 @@ set -e
 INSTALL_MODULES_DIR="output/target/lib/modules"
 INSTALL_IMAGES_DIR="output/images"
 
-cp -r ${TARGET_DIR}/lib/modules/* ${INSTALL_MODULES_DIR}
-cp -v ${BINARIES_DIR}/zImage ${INSTALL_IMAGES_DIR}/kernel.img
+rsync -rv ${TARGET_DIR}/lib/modules/ ${INSTALL_MODULES_DIR}
+cp -v ${BINARIES_DIR}/zImage ${INSTALL_IMAGES_DIR}/kernel7.img
 cp -v ${BINARIES_DIR}/*.dtb ${INSTALL_IMAGES_DIR}
 
 exec board/raspberrypi0/post-build.sh
