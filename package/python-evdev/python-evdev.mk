3d99fb47e6ec305164f7cb2de615884bd7f25951
Hideki Tanji <tanji@daitogiken.com>
2019-12-02 16:15:17 +0900

    最新版。

--- package/python-evdev/python-evdev.mk
+++ package/python-evdev/python-evdev.mk
@@ -4,8 +4,10 @@
 #
 ################################################################################
 
-PYTHON_EVDEV_VERSION = v0.6.4
+PYTHON_EVDEV_VERSION = v1.2.0
 PYTHON_EVDEV_SITE = $(call github,gvalkov,python-evdev,$(PYTHON_EVDEV_VERSION))
 PYTHON_EVDEV_SETUP_TYPE = setuptools
+PYTHON_EVDEV_BUILD_OPTS = build_ecodes --evdev-headers $(HOST_DIR)/$(GNU_TARGET_NAME)/sysroot/usr/include/linux/input.h:$(HOST_DIR)/$(GNU_TARGET_NAME)/sysroot/usr/include/linux/input-event-codes.h
+PYTHON_EVDEV_DEPENDENCIES = linux
 
 $(eval $(python-package))
