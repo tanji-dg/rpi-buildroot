e9d586f54a14b6892681ed695b416f0b24bb3a03
丹治秀樹 <tanji@daitogiken.local>
2020-09-04 11:47:13 +0900

    rpi4対応のため、mainlineからラズパイ用firmwareのパッケージを移植。

--- package/rpi-bt-firmware/rpi-bt-firmware.mk
+++ package/rpi-bt-firmware/rpi-bt-firmware.mk
@@ -4,19 +4,14 @@
 #
 ################################################################################
 
-RPI_BT_FIRMWARE_VERSION = a439f892bf549ddfefa9ba7ad1999cc515f233bf
-RPI_BT_FIRMWARE_SITE = https://aur.archlinux.org/pi-bluetooth.git
-RPI_BT_FIRMWARE_SITE_METHOD = git
+RPI_BT_FIRMWARE_VERSION = d4f7087ecbc8eff9cb64a4650765697157821d64
+RPI_BT_FIRMWARE_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware-rpi,$(RPI_BT_FIRMWARE_VERSION))
 RPI_BT_FIRMWARE_LICENSE = PROPRIETARY
 RPI_BT_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
 
 define RPI_BT_FIRMWARE_INSTALL_TARGET_CMDS
-	ln -sf ../lib/firmware $(TARGET_DIR)/etc/firmware
-	$(INSTALL) -D -m 0644 $(@D)/BCM43430A1.hcd \
-		$(TARGET_DIR)/lib/firmware/BCM43430A1.hcd
-	#$(INSTALL) -D -m 0643 $(@D)/BCM43430A1.hcd $(TARGET_DIR)/lib/firmware/brcm/BCM43430A1.hcd
-	#$(INSTALL) -D -m 0755 package/rpi-bt-firmware/S31bluetooth $(TARGET_DIR)/etc/init.d/S31bluetooth
-	#$(INSTALL) -D -m 0644 package/rpi-bt-firmware/main.conf $(TARGET_DIR)/etc/bluetooth/main.conf
+	$(INSTALL) -d $(TARGET_DIR)/lib/firmware/brcm
+	$(INSTALL) -m 0644 $(@D)/firmware/brcm/*.hcd $(TARGET_DIR)/lib/firmware/brcm
 endef
 
 $(eval $(generic-package))
