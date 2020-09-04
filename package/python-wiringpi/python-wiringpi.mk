7d919ab7ea4d86956d62eba39fea4a2dad1077b8
丹治秀樹 <tanji@daitogiken.local>
2020-09-04 10:50:34 +0900

    RPI4対応のパッケージを追加。

--- package/python-wiringpi/python-wiringpi.mk
+++ package/python-wiringpi/python-wiringpi.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WIRINGPI_VERSION = 2.44.4
+PYTHON_WIRINGPI_VERSION = 2.60.0
 PYTHON_WIRINGPI_SOURCE = wiringpi-$(PYTHON_WIRINGPI_VERSION).tar.gz
-PYTHON_WIRINGPI_SITE = https://pypi.python.org/packages/dd/a5/23352c20c6246608456ca3f34c074d62aed4cdcf7704b62be6246e75881e
+PYTHON_WIRINGPI_SITE = https://files.pythonhosted.org/packages/67/cf/35854ec6c7f7285454e2204a18e977d935aefc99e2b2a14093832faab510
 PYTHON_WIRINGPI_SETUP_TYPE = setuptools
 PYTHON_WIRINGPI_LICENSE = LGPL
 PYTHON_WIRINGPI_LICENSE_FILES = LICENSE.txt
